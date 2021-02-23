0ceef605a55fbd6e6a18f8cf0210d2df5c35b530
Joseph Blais <josephpierreblais@gmail.com>
2021-02-23 17:01:55 -0500

    Add comments for char-counter JS file

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
   $('#tweet-text').on('input', function() {
+    // Get length of text in text-area
     let value = $(this).val();
     let tweetLength = value.length;
-
+    // Update counterNumber by subtracting tweetLength
     let counterNumber = 140;
     counterNumber -= tweetLength;
-    console.log(counterNumber);
-
+    // traverse up DOM tree to get the counter output
     let counter = $(this).closest("form").find(".counter");
+    // update counter output to counterNumber
     counter.text(counterNumber);
   });
 });
