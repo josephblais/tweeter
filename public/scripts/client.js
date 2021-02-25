63f184ac3912f4bb5332d7de334a0b4d1075b1e7
Joseph Blais <josephpierreblais@gmail.com>
2021-02-25 15:58:26 -0500

    Refactor jQuery. Move helper functions out of document.ready for readability

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,53 +6,9 @@
 
 $(document).ready(function() {
   // Render tweets from database on page
-  const renderTweets = (tweets) => {
-    //loop through tweets
-    for (let tweet of tweets) {
-      // calls createTweetElement for each tweet
-      const $tweet = createTweetElement(tweet);
-      // takes return value and appends it to the tweets container
-      $('#tweets-container').prepend($tweet);
-    }
-  };
+  
 
-  // Reencodes potentially unsafe text in tweets to prevent XSS
-  const escapeTweetText = (tweetText) => {
-    let div = document.createElement('div');
-    div.appendChild(document.createTextNode(tweetText));
-    return div.innerHTML;
-  };
-
-  const createTweetElement = (tweetData) => {
-    let $tweet = `<article class="tweet">
-    <header>
-      <div class="username-and-image">
-        <!-- User profile pic / avatar -->
-        <img src="${tweetData.user.avatars}">
-        <!-- User name -->
-        <span>${tweetData.user.name}</span>
-      </div>
-      <div class="user-handle username-and-image">
-        <span>${tweetData.user.handle}</span>
-      </div>
-    </header>
-    <div class="tweet-content">
-      <p>${escapeTweetText(tweetData.content.text)}</p>
-    </div>
-    <footer>
-      <!-- Time posted -->
-      <span>${moment(tweetData.created_at).fromNow()}</span>
-      <!-- logos for flag, retweet, like -->
-        <div class="icons">
-        <img src="docs/flags.png">
-        <img src="docs/retweet.png">
-        <img src="docs/like.png">
-      </div>
-    </footer>
-  </article>`;
-    return $tweet;
-  };
-    
+  loadTweets();
 
   // Posts user-submitted tweet to server
   $('#tweet-form').on('submit', function(event) {
@@ -60,15 +16,12 @@ $(document).ready(function() {
     event.preventDefault();
     // Get the tweet string
     let tweetText = $(this).children("#tweet-text").val();
-
+    
     // Send alerts if tweet is empty or too long
     if (!tweetText) {
-      $('#error-text').text('Empty Tweet! Add some text');
-      // Slide the error message into view
-      $('#error').slideDown();
+      errorDisplay('Empty Tweet! Add some text');
     } else if (tweetText.length > 140) {
-      $('#error-text').text('That Tweet is too long! 140 characters max');
-      $('#error').slideDown();
+      errorDisplay('That Tweet is too long! 140 characters max');
     } else {
       // Serialize the tweet content to submit to database
       const tweet = $(this).serialize();
@@ -94,15 +47,69 @@ $(document).ready(function() {
     }
   });
 
-  // fetches tweets from the server
-  const loadTweets = () => {
-    $.get('/tweets/')
-      .done(function(result) {
-        renderTweets(result);
-      })
-      .fail((err) => console.log(err.message));
-  };
-
-  loadTweets();
-
 });
+
+
+// Helper functions:
+
+// Reencodes potentially unsafe text in tweets to prevent XSS
+const escapeTweetText = (tweetText) => {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(tweetText));
+  return div.innerHTML;
+};
+
+const createTweetElement = (tweetData) => {
+  let $tweet = `<article class="tweet">
+  <header>
+    <div class="username-and-image">
+      <!-- User profile pic / avatar -->
+      <img src="${tweetData.user.avatars}">
+      <!-- User name -->
+      <span>${tweetData.user.name}</span>
+    </div>
+    <div class="user-handle username-and-image">
+      <span>${tweetData.user.handle}</span>
+    </div>
+  </header>
+  <div class="tweet-content">
+    <p>${escapeTweetText(tweetData.content.text)}</p>
+  </div>
+  <footer>
+    <!-- Time posted -->
+    <span>${moment(tweetData.created_at).fromNow()}</span>
+    <!-- logos for flag, retweet, like -->
+      <div class="icons">
+      <img src="docs/flags.png">
+      <img src="docs/retweet.png">
+      <img src="docs/like.png">
+    </div>
+  </footer>
+</article>`;
+  return $tweet;
+};
+  
+// fetches tweets from the server
+const loadTweets = () => {
+  $.get('/tweets/')
+    .done(function(result) {
+      renderTweets(result);
+    })
+    .fail((err) => console.log(err.message));
+};
+
+const renderTweets = (tweets) => {
+  //loop through tweets
+  for (let tweet of tweets) {
+    // calls createTweetElement for each tweet
+    const $tweet = createTweetElement(tweet);
+    // takes return value and appends it to the tweets container
+    $('#tweets-container').prepend($tweet);
+  }
+};
+
+const errorDisplay = (text) => {
+  $('#error-text').text(text);
+  // Slide the error message into view
+  $('#error').slideDown();
+};
